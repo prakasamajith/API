3415c3e502b0cc6338c36dfa9649a54a1d913d87
prakasamajith <prakasamajith@gmail.com>
2024-11-06 16:49:17 +0530

    Login,Statelist,citylist,addaddress,updateaddress

--- src/test/java/com/omrbranch/pojo/sample/Sample_Api.java
+++ src/test/java/com/omrbranch/pojo/sample/Sample_Api.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.omrbranch.baseclass.BaseClass;
+import com.omrbranch.pojo.address.AddUserAddress_Input_Pojo;
+import com.omrbranch.pojo.address.AddUserAddress_Output_Pojo;
 import com.omrbranch.pojo.address.CityList_Input_Pojo;
 import com.omrbranch.pojo.address.StateList;
 import com.omrbranch.pojo.address.StateListAndCityList_Output_Pojo;
+import com.omrbranch.pojo.address.UpdateUserAddress_Input_Pojo;
+import com.omrbranch.pojo.address.UpdateUserAddress_Output_Pojo;
 import com.omrbranch.pojo.login.Login_Output_Pojo;
 import com.omrbranch.pojo.login.UserRole;
-
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
 
 public class Sample_Api extends BaseClass {
 	String stateIdText;
+	String logtoken;
+	int stateIdNum;
+	int cityIdNum;
 
 	@Test(priority = 1)
 	public void login() {
@@ -36,6 +42,7 @@ public class Sample_Api extends BaseClass {
 		int id = login_Output_Pojo.getData().getId();
 		System.out.println(id);
 		Assert.assertEquals(id, 2928, "Verify Id");
+		logtoken = login_Output_Pojo.getData().getLogtoken();
 		ArrayList<UserRole> user_role = login_Output_Pojo.getData().getUser_role();
 		for (UserRole user : user_role) {
 			int id2 = user.getId();
@@ -60,7 +67,7 @@ public class Sample_Api extends BaseClass {
 		for (StateList eachStateList : stateList) {
 			String stateName = eachStateList.getName();
 			if (stateName.equals("Tamil Nadu")) {
-				int stateIdNum = eachStateList.getId();
+				stateIdNum = eachStateList.getId();
 				stateIdText = String.valueOf(stateIdNum);
 				System.out.println(stateIdNum);
 				break;
@@ -92,11 +99,67 @@ public class Sample_Api extends BaseClass {
 		for (StateList eachCityList : cityList) {
 			String cityName = eachCityList.getName();
 			if (cityName.equals("Yercaud")) {
-				int cityIdNum = eachCityList.getId();
+				cityIdNum = eachCityList.getId();
 				System.out.println(cityIdNum);
 				break;
 			}
-			
+
 		}
+	}
+
+	@Test(priority = 4)
+	public void addUserAddress() {
+
+		// 1.Add Headers
+		List<Header> lstHeader = new ArrayList<>();
+		Header h1 = new Header("accept", "application/json");
+		Header h2 = new Header("Authorization", "Bearer " + logtoken);
+		Header h3 = new Header("Content-Type", "application/json");
+		lstHeader.add(h1);
+		lstHeader.add(h2);
+		lstHeader.add(h3);
+		Headers headers = new Headers(lstHeader);
+		addHeaders(headers);
+		// 2.Payload/Req Body
+		AddUserAddress_Input_Pojo addUserAddress_Input_Pojo = new AddUserAddress_Input_Pojo("Prakasam", "Ajith",
+				"9600809646", "PD", stateIdNum, cityIdNum, 100, "636105", "Attur", "Home");
+		addBody(addUserAddress_Input_Pojo);
+		// 3.Request type
+		Response response = addReqType("POST", "https://omrbranch.com/api/addUserAddress");
+		int statusCode = getStatusCode(response);
+		System.out.println(statusCode);
+		AddUserAddress_Output_Pojo addUserAddress_Output_Pojo = response.as(AddUserAddress_Output_Pojo.class);
+		String actMessage = addUserAddress_Output_Pojo.getMessage();
+		int address_id = addUserAddress_Output_Pojo.getAddress_id();
+		System.out.println(address_id);
+		Assert.assertEquals(actMessage, "Address added successfully", "Verify Address added successfully message");
+	}
+
+	@Test(priority = 5)
+
+	public void updateUserAddress() {
+
+		// 1.Add Headers
+		List<Header> lstHeader = new ArrayList<>();
+		Header h1 = new Header("accept", "application/json");
+		Header h2 = new Header("Authorization", "Bearer " + logtoken);
+		Header h3 = new Header("Content-Type", "application/json");
+		lstHeader.add(h1);
+		lstHeader.add(h2);
+		lstHeader.add(h3);
+		Headers headers = new Headers(lstHeader);
+		addHeaders(headers);
+		// 2.Payload/Req Body
+		UpdateUserAddress_Input_Pojo updateUserAddress_Input_Pojo = new UpdateUserAddress_Input_Pojo("95180",
+				"Prakasam", "Ajith", "9600809646", "PD Apartment", stateIdNum, cityIdNum, 100, "636105", "Attur",
+				"Home");
+		addBody(updateUserAddress_Input_Pojo);
+		// 3.Req type
+		Response response = addReqType("PUT", "https://omrbranch.com/api/updateUserAddress");
+		int statusCode = getStatusCode(response);
+		System.out.println(statusCode);
+		UpdateUserAddress_Output_Pojo updateUserAddress_Output_Pojo = response.as(UpdateUserAddress_Output_Pojo.class);
+		String actMessage = updateUserAddress_Output_Pojo.getMessage();
+		Assert.assertEquals(actMessage, "Address updated successfully", "Verify Address updated successfully message");
 	}
 }
